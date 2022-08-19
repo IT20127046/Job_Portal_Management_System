64a11f62a023a16d1a8303e729894e7c4afb3f91
Loshith Hasinda Samararathna <it20127046@my.sliit.lk>
2022-08-19 12:11:47 +0530

    Upload project files to GitHub

---
+++ frontend/src/components/Index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Index() {
+  return (
+    <dvi>
+      <div className="container text-center mt-5">
+        <h1>Job Portal Management System</h1>
+      </div>
+    </dvi>
+  );
+}
