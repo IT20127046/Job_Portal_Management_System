0b3e1ad6abcbbed4cf2610137d56c7b3a0763920
t-w-i-n-k-l-y <hkalani00@gmail.com>
2022-08-30 17:33:37 +0530

    application - backend modifications

--- backend/controllers/applicationController.js
+++ backend/controllers/applicationController.js
@@ -3,7 +3,8 @@ const ApplicationModel = require('../models/applicationModel');
 // save (submit application)
 const save_application = function (req, res){
     let newApplication = new ApplicationModel(req.body);
-
+    // console.log(newApplication);
+    
     newApplication.save((err)=>{
         if(err) {
             return res.status(400).json({
