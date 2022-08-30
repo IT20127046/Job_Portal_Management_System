787031be2258e9f548f793cdb97d78e533530701
t-w-i-n-k-l-y <hkalani00@gmail.com>
2022-08-30 17:36:39 +0530

    Application - frontend with validations

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./components/Index";
+import ApplicationPage from "./components/IT20125202/application/ApplicationPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Index />} />
+        <Route path="/application" element={<ApplicationPage/>} />
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/components/IT20125202/application/ApplicationPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Form from "./Form";
+
+export default function ApplicationPage() {
+  return (
+    <React.Fragment> 
+      <br />
+      <h1 style={{textAlign: "center"}}> Application </h1>
+      <Form/> 
+      
+    </React.Fragment>
+    
+  );
+}
