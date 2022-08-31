9174aaedd981debce2205f17cf4e30e4c7c5497b
t-w-i-n-k-l-y <hkalani00@gmail.com>
2022-08-31 13:09:31 +0530

    view all applications backend impl

--- backend/routes/applicationRoutes.js
+++ backend/routes/applicationRoutes.js
@@ -5,5 +5,13 @@ const ApplicationController = require('../controllers/applicationController');
 // save 
 ApplicationRouter.post('/applications/apply', ApplicationController.save_application);
 
+// get all
+ApplicationRouter.get('/applications', ApplicationController.getAll_applications);
+
+// get received applications according to the company
+ApplicationRouter.get('/applications/received/:id', ApplicationController.getAll_receivedApplications);
+
+// get all submitted applications according to the user
+ApplicationRouter.get('/applications/submitted/:id', ApplicationController.getAll_submittedApplications);
 
 module.exports = ApplicationRouter;
