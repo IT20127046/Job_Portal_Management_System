f4abd9317c45fc20e4cdb8debdce2fe2d111ba58
t-w-i-n-k-l-y <hkalani00@gmail.com>
2022-08-31 20:45:57 +0530

    viewall applications - frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./components/Index";
 import ApplicationPage from "./components/IT20125202/application/ApplicationPage";
+import AllApplications from "./components/IT20125202/allApplications/AllApplications";
+import ApplicationDetails from "./components/IT20125202/applicationDetails/ApplicationDetails";
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Index />} />
         <Route path="/application" element={<ApplicationPage/>} />
+        <Route path="/all_applications" element={<AllApplications/>} />
+        <Route path="/application_details/:id" element={<ApplicationDetails/>} />
       </Routes>
     </BrowserRouter>
   );
