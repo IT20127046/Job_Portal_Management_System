aa43aa554d3cd9d4e89f9caa79a57ecac09bd849
t-w-i-n-k-l-y <hkalani00@gmail.com>
2022-09-01 21:47:43 +0530

    application details view - frontend

--- frontend/src/components/IT20125202/applicationDetails/ApplicationDetails.jsx
+++ frontend/src/components/IT20125202/applicationDetails/ApplicationDetails.jsx
@@ -1,7 +1,35 @@
-import React from 'react'
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import RecruiterView from './RecruiterView';
+import JobSeekerView from './JobSeekerView';
 
 export default function ApplicationDetails() {
+
+  let { id } = useParams();
+  const userType = 'recruiter'; //need to get from the session
+  // const userType = 'jobseeker';
+
+  React.useEffect(() => {
+    document.title = "All Application";
+    // console.log(id);
+
+  }, []);
+
   return (
-    <div>Application Details</div>
+    <div>
+      <br />
+      <h1 style={{ textAlign: "center" }}> Application Details </h1>
+      {
+        userType === 'recruiter' && (
+          RecruiterView(id)
+        )
+      }
+      {
+        userType === 'jobseeker' && (
+          JobSeekerView(id)
+        )
+      }
+
+    </div>
   )
 }
