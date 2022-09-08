25356e29f6207137d3e1ca61a2fb257c84b31b49
t-w-i-n-k-l-y <hkalani00@gmail.com>
2022-09-08 10:53:54 +0530

    application - mod 2

--- frontend/src/components/IT20125202/applicationDetails/ApplicationDetails.jsx
+++ frontend/src/components/IT20125202/applicationDetails/ApplicationDetails.jsx
@@ -4,7 +4,7 @@ import jwt_decode from 'jwt-decode';
 import RecruiterView from './RecruiterView';
 import JobSeekerView from './JobSeekerView';
 import NavBar from '../../IT20128036/NavBar';
-import image from "../../../images/background.png";
+import image from "../../../images/back1.jpg";
 
 export default function ApplicationDetails() {
 
@@ -22,23 +22,25 @@ export default function ApplicationDetails() {
   }, []);
 
   return (
-    <div className='jumbotron' style={{ paddingLeft: '50px', paddingRight: '50px', paddingBottom: '50px', paddingTop: '10px', backgroundImage: `url(${image})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover', margin: '0px 0px 0px 0px', overflowY: 'scroll', height: '100vh' }}>
+    <div>
       <NavBar />
-      <div className='jumbotron' style={{ background: 'white', borderTop: '5px solid black', minHeight: '100vh' }}>
-        <br />
-        <br />
-        <h1 style={{ textAlign: "center" }}> Application Details </h1>
-        {
-          userType === 'Job Recruiter' && (
-            <RecruiterView />
-          )
-        }
-        {
-          userType === 'Job Seeker' && (
-            <JobSeekerView />
-          )
-        }
+      <div className='jumbotron' style={{ paddingLeft: '50px', paddingRight: '50px', paddingBottom: '50px', paddingTop: '10px', backgroundImage: `url(${image})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover', margin: '0px 0px 0px 0px', overflowY: 'scroll', height: '100vh', borderTop: '5px solid black' }}>
+        <div className='jumbotron' style={{ background: 'white', minHeight: '100vh' }}>
+          <br />
+          <br />
+          <h1 style={{ textAlign: "center" }}> Application Details </h1>
+          {
+            userType === 'Job Recruiter' && (
+              <RecruiterView />
+            )
+          }
+          {
+            userType === 'Job Seeker' && (
+              <JobSeekerView />
+            )
+          }
 
+        </div>
       </div>
     </div>
   )
