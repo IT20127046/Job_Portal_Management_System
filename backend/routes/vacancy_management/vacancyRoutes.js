d9bc5231f49458458964e11977234f718df49a98
IT20131456 <it20131456@my.sliit.lk>
2022-09-22 19:11:11 +0530

    image  upload

--- backend/routes/vacancy_management/vacancyRoutes.js
+++ backend/routes/vacancy_management/vacancyRoutes.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const VacancyRouter = express.Router();
+const multer = require("multer");
 const VacancyController = require("../../controllers/vacancy_management/vacancyControllers");
 
+//Save image uisng multer
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, "./uploads");
+  },
+  filename: (req, file, callback) => {
+    callback(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+VacancyRouter.post("/vacancy/add", upload.single("image"),VacancyController.save_vacancy);
 // Save Vacancy
-VacancyRouter.post("/vacancy/add", VacancyController.save_vacancy);
+// VacancyRouter.post("/vacancy/add", VacancyController.save_vacancy);
 
 // GetAll Vacancy
 VacancyRouter.get("/vacancy/getAll", VacancyController.getAll_vacancy);
@@ -18,6 +32,9 @@ VacancyRouter.put("/vacancy/update/:id", VacancyController.update_vacancy);
 VacancyRouter.delete("/vacancy/delete/:id", VacancyController.delete_vacancy);
 
 // Get Vacancy By Name
-VacancyRouter.get("/vacancy/get/name/:name", VacancyController.getVacancyByName);
+VacancyRouter.get(
+  "/vacancy/get/name/:name",
+  VacancyController.getVacancyByName
+);
 
 module.exports = VacancyRouter;
